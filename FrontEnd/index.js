bfe8c72bcce6c856fc0668ff406a54a29fdbe9dc
IsidoreSko <127843208+IsidoreSko@users.noreply.github.com>
2023-06-16 12:27:32 +0200

    Ajout des boutons catégories retouche

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,9 +1,17 @@
-// Création des constantes:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Création de listes:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 const allWorks = new Set();
 const allCategories = new Set();
 
 // Fonction "init"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// création du bouton de toutes les catégories:
+let buttonCategory = document.createElement("button");
+let allFilters = document.querySelector(".allFilters");
+buttonCategory.classList.add(`button`, "filter");
+buttonCategory.setAttribute("id", "0");
+buttonCategory.type = "button";
+buttonCategory.innerHTML = "Tous";
+allFilters.appendChild(buttonCategory);
 
 async function init() {
   // Pour récupérer les données des "travaux":
@@ -13,12 +21,23 @@ async function init() {
   }
   // Pour récupérer les données des "catégories":
   const categories = await getAlldatabaseInfo("categories");
+  displayWorks();
   for (const category of categories) {
     allCategories.add(category);
+    // console.log(category.id);
+    allCategories.add("tous");
+    console.log(allCategories);
+    // Création des boutons pour les catégories:
+    let buttonCategory = document.createElement("button");
+    let allFilters = document.querySelector(".allFilters");
+    buttonCategory.classList.add(`button`, "filter");
+    buttonCategory.setAttribute("id", `${category.id}`);
+    buttonCategory.type = "button";
+    buttonCategory.innerHTML = category.name;
+    allFilters.appendChild(buttonCategory);
   }
-  displayWorks();
 }
-
+// ${category.name}
 init();
 
 // Fonction pour les base de données::::::::::::::::::::::::::::::::::::::::::::::
@@ -34,13 +53,14 @@ async function getAlldatabaseInfo(type) {
 }
 
 // Fonction pour l'affichage des travaux:
-function displayWorks() {
+async function displayWorks() {
   // Récupération de l'élément du DOM qui accueillera les travaux:
   const allFigures = document.querySelector(".gallery");
   // Et effacer son contenu du fichier HTML:
   allFigures.innerHTML = "";
   // for (let i = 0; i < allWorks.size; i++)
   for (const work of allWorks) {
+    // console.log(work.categoryId);
     // Création d’une balise dédiée chaque travail:
     let figureElement = document.createElement("figure");
     // On crée l’élément img. :
@@ -60,24 +80,8 @@ function displayWorks() {
   }
 }
 
-// Ajout des événements au click des boutons de filtre:
-
-// Récupération des boutons :
-
-const buttonObjects = document.querySelector(".buttonObjects");
-const buttonApartments = document.querySelector(".buttonApartments");
-const buttonHotels = document.querySelector(".buttonHotels");
-
-buttonObjects.addEventListener("click", () => {
-  console.log("oui");
-});
-// for (category of allCategories) {
-//   let category = category.filter(object);
-// }
-buttonApartments.addEventListener("click", () => {
-  console.log("ouioui");
-});
-
-buttonHotels.addEventListener("click", () => {
-  console.log("ouiouioui");
-});
+// Création de la "div" allFilters:
+// let allFilters = document.createElement("div");
+// let portfolio = document.getElementById("portfolio");
+// allFilters.classList.add("allFilters");
+// portfolio.appendChild(allFilters);
