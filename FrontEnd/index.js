f9a4d2def4cd48b648fee690576b4ae1731db321
IsidoreSko <127843208+IsidoreSko@users.noreply.github.com>
2023-06-20 15:17:27 +0200

    Filtres

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,17 +1,9 @@
-// Création de listes:::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+// Création de listes:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 const allWorks = new Set();
 const allCategories = new Set();
 
 // Fonction "init"::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-// création du bouton de toutes les catégories:
-let buttonCategory = document.createElement("button");
-let allFilters = document.querySelector(".allFilters");
-buttonCategory.classList.add(`button`, "filter");
-buttonCategory.setAttribute("id", "0");
-buttonCategory.type = "button";
-buttonCategory.innerHTML = "Tous";
-allFilters.appendChild(buttonCategory);
 
 async function init() {
   // Pour récupérer les données des "travaux":
@@ -21,26 +13,17 @@ async function init() {
   }
   // Pour récupérer les données des "catégories":
   const categories = await getAlldatabaseInfo("categories");
-  displayWorks();
+
   for (const category of categories) {
     allCategories.add(category);
-    // console.log(category.id);
-    allCategories.add("tous");
-    console.log(allCategories);
-    // Création des boutons pour les catégories:
-    let buttonCategory = document.createElement("button");
-    let allFilters = document.querySelector(".allFilters");
-    buttonCategory.classList.add(`button`, "filter");
-    buttonCategory.setAttribute("id", `${category.id}`);
-    buttonCategory.type = "button";
-    buttonCategory.innerHTML = category.name;
-    allFilters.appendChild(buttonCategory);
   }
+  displayWorks();
+  displayFiltres();
 }
-// ${category.name}
+
 init();
 
-// Fonction pour les base de données::::::::::::::::::::::::::::::::::::::::::::::
+// Fonction pour les bases de données::::::::::::::::::::::::::::::::::::::::::::::
 
 // Pour récupérer les données selon leur type:
 async function getAlldatabaseInfo(type) {
@@ -53,35 +36,97 @@ async function getAlldatabaseInfo(type) {
 }
 
 // Fonction pour l'affichage des travaux:
-async function displayWorks() {
+async function displayWorks(id = "0") {
   // Récupération de l'élément du DOM qui accueillera les travaux:
   const allFigures = document.querySelector(".gallery");
   // Et effacer son contenu du fichier HTML:
   allFigures.innerHTML = "";
-  // for (let i = 0; i < allWorks.size; i++)
   for (const work of allWorks) {
-    // console.log(work.categoryId);
-    // Création d’une balise dédiée chaque travail:
-    let figureElement = document.createElement("figure");
-    // On crée l’élément img. :
-    let imageElement = document.createElement("img");
-    // On crée l’élément "figcaption" :
-    let figcaptionElement = document.createElement("figcaption");
-    // On accède à l’indice i de la liste des travaux pour configurer la source de l’image.
-    imageElement.src = work.imageUrl;
-    // // Et la balise "figcation":
-    figcaptionElement.innerText = work.title;
-    // On rattache la balise "figure" à la section "allFigure":
-    allFigures.appendChild(figureElement);
-    // On rattache l’image à l'élément "figure":
-    figureElement.appendChild(imageElement);
-    // On rattache l’image à l'élément "figcation":
-    figureElement.appendChild(figcaptionElement);
+    if (id == work.categoryId || id == "0") {
+      // Création d’une balise dédiée chaque travail:
+      let figureElement = document.createElement("figure");
+      // On crée l’élément img. :
+      let imageElement = document.createElement("img");
+      // On crée l’élément "figcaption" :
+      let figcaptionElement = document.createElement("figcaption");
+      // Ajout d'un "id" suivant la catégorie:
+      figureElement.setAttribute("id", `${work.categoryId}`);
+      // On accède à l’indice i de la liste des travaux pour configurer la source de l’image.
+      imageElement.src = work.imageUrl;
+      // // Et la balise "figcation":
+      figcaptionElement.innerText = work.title;
+      // On rattache la balise "figure" à la section "allFigure":
+      allFigures.appendChild(figureElement);
+      // On rattache l’image à l'élément "figure":
+      figureElement.appendChild(imageElement);
+      // On rattache l’image à l'élément "figcation":
+      figureElement.appendChild(figcaptionElement);
+    }
   }
 }
 
-// Création de la "div" allFilters:
-// let allFilters = document.createElement("div");
-// let portfolio = document.getElementById("portfolio");
-// allFilters.classList.add("allFilters");
-// portfolio.appendChild(allFilters);
+// Fonction pour l'affichage des boutons filtres:
+async function displayFiltres() {
+  // Récupération de l'élément du DOM qui accueillera les boutons:
+  const allFilters = document.querySelector(".allFilters");
+  // Et effacer son contenu du fichier HTML:
+  allFilters.innerHTML = "";
+  // Créaion du bouton "Tous":
+  const allBtn = document.createElement("button");
+  // Ajout des "class" sur le bouton:
+  allBtn.classList.add("buttonCategory", "filter", "active");
+  // Ajout du texte du bouton:
+  allBtn.textContent = "Tous";
+  // Ajout d'un "id" pour le bouton:
+  allBtn.dataset.id = "0";
+
+  const fragment = document.createDocumentFragment();
+  fragment.appendChild(allBtn);
+  for (const category of allCategories) {
+    // // Création d’une balise dédiée chaque bouton:
+    const buttonCategory = document.createElement("button");
+    // Ajout des "class" des boutons:
+    buttonCategory.classList.add("buttonCategory", "filter");
+    // Ajout des "id" des boutons:
+    buttonCategory.dataset.id = category.id;
+    // Ajout du nom de la catégorie des boutons:
+    buttonCategory.textContent = category.name;
+    // Ratachement des balises "buton" au parent:
+    fragment.appendChild(buttonCategory);
+  }
+  allFilters.appendChild(fragment);
+  createFilterListener();
+}
+
+// Fonction pour la réalisation des filtres:
+function createFilterListener() {
+  // Crétion d'une constante qui récupère tous les boutons:
+  const allBtns = document.querySelectorAll(".filter");
+
+  for (const buttonCategory of allBtns) {
+    buttonCategory.addEventListener("click", (e) => {
+      // Création d'une constante pour l'événement sur le bouton cliqué:
+      const clickedBtn = e.target;
+      // Création d'une constante pour le bouton cliqué:
+      const selectedId = clickedBtn.dataset.id;
+      // Supréssion de la "class" "active" du bouton:
+      document.querySelector(".active").classList.remove("active");
+      // Ajout de la "class" "active" sur le bouton cliqué:
+      clickedBtn.classList.add("active");
+
+      switch (selectedId) {
+        case "1":
+          displayWorks(1);
+          break;
+        case "2":
+          displayWorks(2);
+          break;
+        case "3":
+          displayWorks(3);
+          break;
+        default:
+          displayWorks();
+      }
+    });
+  }
+}
